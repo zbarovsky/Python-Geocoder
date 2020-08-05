3ec5525a0f66a369c8f2e50123cc61b3ac9c44a3
zbarovsky <zachary.barovsky@gmail.com>
2020-08-04 19:02:25 -0700

    created app folder to rebuild hw without looking at solution brach

---
+++ app.py
@@ -0,0 +1,17 @@
+import geocoder
+
+destinations = [
+    "Space Needle",
+    "Crater Lake",
+    "Golden Gate Bridge",
+    "Yosemite National Park",
+    "Las Vegas, Nevada",
+    "Grand Canyon National Park",
+    "Aspen, Colorado",
+    "Mount Rushmore",
+    "Yellowstone National Park",
+    "Sandpoint, Idaho",
+    "Banff National Park",
+    "Capilano Suspension Bridge"
+]
+
